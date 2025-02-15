3a3d50a37c926ff40a0d054c1cb3fe187279076d
dimXmanyu <130482773+dimXmanyu@users.noreply.github.com>
2025-02-15 21:55:09 +0800

    Update hardhat.config.ts

--- darc-protocol/hardhat.config.ts
+++ darc-protocol/hardhat.config.ts
@@ -23,3 +23,82 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      viaIR: true,
+      optimizer: { 
+        enabled: true,
+        runs: 1,
+        details: {
+           yul: true,
+        }
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    }
+  }
+};
+
+export default config;
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      viaIR: true,
+      optimizer: { 
+        enabled: true,
+        runs: 1,
+        details: {
+           yul: true,
+        }
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    }
+  }
+};
+
+export default config;
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      viaIR: true,
+      optimizer: { 
+        enabled: true,
+        runs: 1,
+        details: {
+           yul: true,
+        }
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    }
+  }
+};
+
+export default config;
+
